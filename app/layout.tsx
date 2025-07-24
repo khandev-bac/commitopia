f793e6a893cb7b89ab28d1f40f6434cecf0a5850
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-07-24 10:29:20 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
-import NextAuth from "next-auth";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
